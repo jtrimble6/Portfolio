e8d5f230b750f1308116b218e58e6877489736d1
Joshua Trimble <jtrimble6@yahoo.com>
2018-10-18 16:32:07 -0500

    working portfolio

---
+++ portfolio/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PortfolioContainer from "./components/PortfolioContainer";
+import './App.css';
+
+const App = () => <PortfolioContainer />
+
+// const App = () => {
+//   <Router>
+//     <div>
+//       <NavTabs />
+//       <Route exact path="/" component={Home} />
+//       <Route exact path="/about" component={About} />
+//       <Route exact path="/projects" component={Projects} />
+//       <Route exact path="/contact" component={Contact} />
+//     </div>
+//   </Router>
+// }
+  
+export default App;
